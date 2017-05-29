1273582f6e41ddb7118342080cb4fa07f45e8224
Yashan Singh <yashan@users.noreply.github.com>
2017-05-29 12:58:03 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact Us Form</title>
+</head>
+<body>
+<h1>Contact Us Form</h1>
+<p>This is a free contact us form availabl for anyone to use. This Form can be used to send data by email to the owner or managers of the website, it will allow to save data and messages in a MySQL database by using the different files provided.</p>
+<h3>Form</h3>
+<form action="" method="post"></form>
+<label for="name">NAME</label><input type="TEXT" name="name" maxlength="30">
+<label for="email">EMAIL</label><input type="email" name="email" placeholder="You@provider.com">
+<label for="Phone">Phone Number</label><input type="" name="Phone">
+<label for="subject">Subject</label><input type="TEXT" name="subject" maxlength="30">
+<textarea rows="5" cols="40"></textarea>
+<input type="Submit" name="submit">
+
+</body> 
+</html>
